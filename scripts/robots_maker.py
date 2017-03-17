1fc6a4f3239fe31346dad03d7b427ba32bc8299a
Philipp Schnattinger <philipp.schnattinger@ipa.fraunhofer.de>
2017-03-17 16:04:05 +0100

    changed robot_maker for localization

--- scripts/robots_maker.py
+++ scripts/robots_maker.py
@@ -12,26 +12,31 @@ class robot_maker():
     def __init__(self):
         self.int_res = [0.05, 0.1, 0.25, 0.5, 0.75, 1.0, 1.25, 1.5]
         self.ndt_mapping = [True, False]
-        self.neighbourhood_score = [0.5, 1]
-        self.number_of_neighbours = [4, 8]
+        self.ndt_matching = [False]
         self.observation_model = ["gaussianRange","standard"]
         self.standard_deviation_range = [0.02]
-        self.file_path = '/home/srd-ps/git/atf_ws/src/long_term_slam_test/config/robots/'
+        self.file_path = '/home/srd-ps/git/test_ws/src/long_term_slam_test/config/robots/'
         self.robots = ""
 
 
     def make_robots(self):
-        for obs in self.observation_model:
-            for ndt in self.ndt_mapping:
-                for neighbourhood in self.neighbourhood_score:
+        for n in range(0, 2):
+            for obs in self.observation_model:
+                for ndt in self.ndt_mapping:
                     for res in self.int_res:
-                        for neighbours in self.number_of_neighbours:
+                        for matching in self.ndt_matching:
                             for dev in self.standard_deviation_range:
-                                if (ndt ==True):
-                                    ndt_str ="ndt"
+                                if (ndt == False and matching == True):
+                                    continue
+                                if (ndt == True):
+                                    ndt_str ="ndt_mapping"
                                 else:
-                                    ndt_str="no_ndt"
-                                filename = str(obs)+"_"+str(res)+"m_"+ndt_str+"_"+str(neighbours)+"_"+str(neighbourhood)
+                                    ndt_str="no_ndt_mapping"
+                                if (matching ==True):
+                                    ndt_match_str ="ndt_matching"
+                                else:
+                                    ndt_match_str="no_ndt_matching"
+                                filename = str(obs)+"_"+str(res)+"m_"+ndt_str+"_"+str(ndt_match_str)+"_"+str(dev)+"_"+str(n)
                                 # save everything in the file
                                 #with open(self.file_path+filename, 'w') as stream:
                                 print "Path: "+self.file_path+filename
@@ -39,7 +44,7 @@ class robot_maker():
                                 robot.write("robot_bringup_launch: \"launch/all.launch\"\nwait_for_topics: []\nwait_for_services: []\n"
                                              "additional_parameters:\n  \"/use_sim_time\": true\n"
                                              "additional_arguments:\n  \"int_res\": "+str(res)+"\n  \"standard_deviation_range\": "+str(dev)+
-                                             "\n  \"static_map\": true\n  \"ndt_mapping\": " +str(ndt) + "\n  \"observation_model\": "+str(obs) +"\n  \"number_of_neighbours\": "+str(neighbours) + "\n  \"neighbourhood_score\": "+str(neighbourhood)+ "\n")
+                                             "\n  \"static_map\": true\n  \"ndt_mapping\": " +str(ndt) + "\n  \"observation_model\": "+str(obs) +"\n  \"ndt_matching\": "+str(matching) + "\n  \"eval_file_name\": " + str(obs) + "_" + str(res)+ "_" + ndt_str +"_"+ ndt_match_str +"_"+str(dev)+"_"+"run_"+str(n)+ "\n")
                                     # stream.write(yaml.dump({'path_length': {'topics': [{'"/tf"', '"/scan_unified"'}]
                                     #                         'robot_bringup_launch': '"launch/all.launch"'
                                     #                         'wait_for_topics': '[]'
